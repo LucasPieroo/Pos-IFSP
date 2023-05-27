72f5ae69d4212f7cd1c9854e564be0c2c13d3191
LucasPieroo <87786992+LucasPieroo@users.noreply.github.com>
2023-05-27 17:37:08 -0300

    Update interface.py

--- D3TOP/interface.py
+++ D3TOP/interface.py
@@ -62,12 +62,12 @@ base_auxiliar["ordem"] = pd.Categorical(base_auxiliar["workId"], categories=simi
 base_auxiliar = base_auxiliar.sort_values("ordem").reset_index()
 
 if fuzzy_usar == "Yes":
-    mask = base_auxiliar["name"].apply(lambda x: fuzz.token_sort_ratio(option, x) < 75)
+    mask = base_auxiliar["name"].apply(lambda x: fuzz.partial_ratio(option, x) < 75)
     filtered_df = base_auxiliar[mask]
     base_auxiliar = filtered_df.reset_index(drop = True)
 
 # Calculate Fuzzy Ratio for each row
-base_auxiliar['fuzzy_ratio'] = base_auxiliar['genres'].apply(lambda x: fuzz.ratio(x, genero))
+base_auxiliar['fuzzy_ratio'] = base_auxiliar['genres'].apply(lambda x: fuzz.token_sort_ratio(x, genero))
 
 # Create categories based on Fuzzy Ratio
 conditions = [
